43399ff52b01bd48e3add44240fccaa0c33ebb21
Aljoscha Nyang <a.nyang@gmx.de>
2023-11-22 12:02:42 +0100

    Accept just one exercise

--- frontend/src/pages/AddPage.tsx
+++ frontend/src/pages/AddPage.tsx
@@ -1,42 +1,8 @@
 import "./AddPage.css";
 import {useNavigate} from "react-router-dom";
-import React, {useState} from "react";
-
-type Exercise = {
-    id: number,
-    name:string,
-    sets: number,
-    reps: number,
-    weight: number,
-    break: number
-}
 
 function AddPage() {
     const navigate = useNavigate();
-    const [name, setName] = useState<string>("");
-    const [sets, setSets] = useState(0);
-    const [reps, setReps] =useState(0);
-    const [weight, setWeight] = useState(0);
-    const [breakTime, setBreakTime] = useState(0);
-    const [exercises, setExercises] = useState<Exercise[]>([]);
-    const [nextId, setNextId] = useState(0);
-    function addNewExerciseForm() {
-        setExercises([...exercises, {
-            id: nextId,
-            name:"",
-            sets: 0,
-            reps: 0,
-            weight: 0,
-            break: 0
-            }
-        ]);
-        setNextId(nextId+1);
-    }
-
-    function deleteExerciseForm(id:number) {
-        const newExercises = exercises.filter((exercise:Exercise) => exercise.id !== id);
-        setExercises(newExercises);
-    }
 
     function handleSubmit(event: React.FormEvent<HTMLFormElement>):void {
         event.preventDefault();
@@ -55,34 +21,27 @@ function AddPage() {
                         <label>Name <input type={"text"} /> </label><br/>
                         <label>Description <input type={"text"} /> </label><br/>
                         <label>Day <input type={"text"} /> </label><br/>
-                        {exercises.map(exercise => {
-                            return <fieldset key={exercise.id}>
-                                <legend>Exercise</legend>
-                                <label>
-                                    Name
-                                    <input
-                                        type={"text"}
-                                        defaultValue={exercise.name}
-                                    />
-                                </label><br/>
-                                <label>Sets <input type={"number"} defaultValue={exercise.sets}/></label><br/>
-                                <label>Reps <input type={"number"} defaultValue={exercise.reps}/></label><br/>
-                                <label>Weight <input type={"number"} defaultValue={exercise.weight}/></label><br/>
-                                <label>Break <input type={"number"} defaultValue={exercise.break}/></label>
-                                <button type={"button"} onClick={() => deleteExerciseForm(exercise.id)}>Delete</button>
-                            </fieldset>
-                        })
-                        }
-                        <button type={"button"} onClick={addNewExerciseForm}>Add Exercise</button>
+                        <fieldset>
+                            <legend>Exercise</legend>
+                            <label>
+                                Name
+                                <input
+                                    type={"text"}
+                                />
+                            </label><br/>
+                            <label>Sets <input type={"number"} /></label><br/>
+                            <label>Reps <input type={"number"} /></label><br/>
+                            <label>Weight <input type={"number"} /></label><br/>
+                            <label>Break <input type={"number"} /></label>
+                        </fieldset>
                     </form>
-
                 </main>
             </div>
             <div className={"position-fix"}></div>
             <div className={"container"}>
                 <footer className={"footer-add-page"}>
-                        <button onClick={() => navigate("/")}>0</button>
-                        <button>1</button>
+                        <button onClick={() => navigate("/")}>cancel</button>
+                        <button>save</button>
                 </footer>
             </div>
         </>
