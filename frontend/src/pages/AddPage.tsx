d67900f5c5c90077541c166c5f3f90c16358dc03
Aljoscha Nyang <a.nyang@gmx.de>
2023-11-22 14:18:36 +0100

    Add axios dependency, implement a form to add a workout to the database

--- frontend/src/pages/AddPage.tsx
+++ frontend/src/pages/AddPage.tsx
@@ -1,11 +1,63 @@
 import "./AddPage.css";
 import {useNavigate} from "react-router-dom";
+import React, {useState} from "react";
+import axios from "axios";
+
+type WorkoutExercise = {
+    name: string,
+    setCount: number,
+    repsPerSet: number,
+    weightInKg: number;
+    breakInSec: number
+}
+
+type Workout = {
+    userId: string,
+    workoutName: string,
+    workoutDay: string,
+    description: string,
+    plan: WorkoutExercise[]
+}
 
 function AddPage() {
     const navigate = useNavigate();
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [day, setDay] = useState("");
+    const [exerciseName, setExerciseName] = useState("");
+    const [sets, setSets] = useState(0);
+    const [reps, setReps] = useState(0);
+    const [weight, setWeight] = useState(0);
+    const [breakTime, setBreakTime] = useState(0);
+
+    function addWorkout() {
+        const newWorkoutData:Workout = {
+            userId:"655b5b283f332f4fcfbf02c0",
+            workoutName: name,
+            workoutDay:day,
+            description: description,
+            plan: [
+                {
+                    name: exerciseName,
+                    setCount: sets,
+                    repsPerSet: reps,
+                    weightInKg: weight,
+                    breakInSec: breakTime
+                }
+            ]
+        }
+
+        axios.post('/api/workouts', newWorkoutData)
+            .then(() => console.log("Workout saved"))
+            .catch(error => {
+                console.error('Error adding data:', error);
+            });
+    }
 
     function handleSubmit(event: React.FormEvent<HTMLFormElement>):void {
         event.preventDefault();
+        addWorkout();
+        navigate("/");
     }
 
     return (
@@ -17,22 +69,69 @@ function AddPage() {
             <div className={"position-fix"}></div>
             <div className={"container"}>
                 <main className={"main-add-page"}>
-                    <form onSubmit={handleSubmit}>
-                        <label>Name <input type={"text"} /> </label><br/>
-                        <label>Description <input type={"text"} /> </label><br/>
-                        <label>Day <input type={"text"} /> </label><br/>
+                    <form id={"workout-form"} onSubmit={handleSubmit}>
+                        <label>
+                            Name
+                            <input
+                                type={"text"}
+                                value={name}
+                                onChange={(event) => setName(event.target.value)}
+                            />
+                        </label><br/>
+                        <label>
+                            Description
+                            <input
+                                type={"text"}
+                                value={description}
+                                onChange={(event) => setDescription(event.target.value)}
+                            />
+                        </label><br/>
+                        <label>
+                            Day
+                            <input
+                                type={"text"}
+                                value={day}
+                                onChange={(event) => setDay(event.target.value)}
+                            />
+                        </label><br/>
+
                         <fieldset>
                             <legend>Exercise</legend>
-                            <label>
-                                Name
+                            <label>Name
                                 <input
                                     type={"text"}
+                                    value={exerciseName}
+                                    onChange={(event) => setExerciseName(event.target.value)}
+                                /> {exerciseName}
+                            </label><br/>
+                            <label>Sets
+                                <input
+                                    type={"number"}
+                                    value={sets}
+                                    onChange={(event) => setSets(parseInt(event.target.value))}
                                 />
                             </label><br/>
-                            <label>Sets <input type={"number"} /></label><br/>
-                            <label>Reps <input type={"number"} /></label><br/>
-                            <label>Weight <input type={"number"} /></label><br/>
-                            <label>Break <input type={"number"} /></label>
+                            <label>Reps
+                                <input
+                                    type={"number"}
+                                    value={reps}
+                                    onChange={(event) => setReps(parseInt(event.target.value))}
+                                />
+                            </label><br/>
+                            <label>Weight
+                                <input
+                                    type={"number"}
+                                    value={weight}
+                                    onChange={(event) => setWeight(parseInt(event.target.value))}
+                                />
+                            </label><br/>
+                            <label>Break
+                                <input
+                                    type={"number"}
+                                    value={breakTime}
+                                    onChange={(event) => setBreakTime(parseInt(event.target.value))}
+                                />
+                            </label>
                         </fieldset>
                     </form>
                 </main>
@@ -41,7 +140,7 @@ function AddPage() {
             <div className={"container"}>
                 <footer className={"footer-add-page"}>
                         <button onClick={() => navigate("/")}>cancel</button>
-                        <button>save</button>
+                        <button type={"submit"} form={"workout-form"}>save</button>
                 </footer>
             </div>
         </>
