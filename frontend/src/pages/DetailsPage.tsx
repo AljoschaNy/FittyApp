e9e3b0a9ef1f69522be3957101b8b5c9fb79ad27
Aljoscha Nyang <a.nyang@gmx.de>
2023-11-23 15:24:12 +0100

    Add home button to DetailsPage

--- frontend/src/pages/DetailsPage.tsx
+++ frontend/src/pages/DetailsPage.tsx
@@ -1,7 +1,12 @@
+import {useNavigate} from "react-router-dom";
+
 function DetailsPage() {
+    const navigate = useNavigate();
+
     return (
         <>
             <h2>Details Page</h2>
+            <button onClick={() => navigate("/")}>Home</button>
         </>
     )
 }
