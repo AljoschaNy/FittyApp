f3b0d93e45a78e300ff2ac5fd477a8877b73d774
Aljoscha Nyang <a.nyang@gmx.de>
2023-11-24 17:12:40 +0100

    Create FooterFormPages component and add some styles

---
+++ frontend/src/components/footer/FooterFormPages.tsx
@@ -0,0 +1,21 @@
+import "./FooterFormPages.css";
+import {useNavigate} from "react-router-dom";
+import {FooterFormPagesType} from "../../types/types.ts";
+
+function FooterFormPages({cancelDestination, formId}:Readonly<FooterFormPagesType>) {
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <div className={"position-fix-bottom"}></div>
+            <div className={"container"}>
+                <footer className={"footer-form-pages"}>
+                    <button onClick={() => navigate(cancelDestination)}>cancel</button>
+                    <button type={"submit"} form={formId}>save</button>
+                </footer>
+            </div>
+        </>
+    );
+}
+
+export default FooterFormPages;
