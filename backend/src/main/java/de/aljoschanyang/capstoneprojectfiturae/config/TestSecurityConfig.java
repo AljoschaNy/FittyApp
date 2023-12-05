10276d64cf354a44bee812808fcf4a852efc11ee
Aljoscha Nyang <a.nyang@gmx.de>
2023-12-05 13:43:13 +0100

    Create TestSecurityConfig

---
+++ backend/src/main/java/de/aljoschanyang/capstoneprojectfiturae/config/TestSecurityConfig.java
@@ -0,0 +1,25 @@
+package de.aljoschanyang.capstoneprojectfiturae.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Profile("test")
+@Configuration
+@EnableWebSecurity
+public class TestSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
