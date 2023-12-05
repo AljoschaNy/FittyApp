b4a3d2278dd2d65e6c7c6961ae6c46e003ae207f
Aljoscha Nyang <a.nyang@gmx.de>
2023-12-05 20:08:10 +0100

    Fix SecurityConfig

--- backend/src/main/java/de/aljoschanyang/capstoneprojectfiturae/config/SecurityConfig.java
+++ backend/src/main/java/de/aljoschanyang/capstoneprojectfiturae/config/SecurityConfig.java
@@ -3,12 +3,14 @@ package de.aljoschanyang.capstoneprojectfiturae.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Profile("prod")
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
