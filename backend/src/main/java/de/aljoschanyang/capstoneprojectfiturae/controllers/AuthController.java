0d4e1b734544cd7323e2cb7c47ef4c149248e0c1
Aljoscha Nyang <a.nyang@gmx.de>
2023-12-07 00:12:19 +0100

    Create AuthController

---
+++ backend/src/main/java/de/aljoschanyang/capstoneprojectfiturae/controllers/AuthController.java
@@ -0,0 +1,21 @@
+package de.aljoschanyang.capstoneprojectfiturae.controllers;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @GetMapping("/me")
+    public String getMe() {
+        var principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(principal instanceof DefaultOAuth2User defaultOAuth2User) {
+            return defaultOAuth2User.getAttributes().get("login").toString();
+        }
+        return "User";
+    }
+}
