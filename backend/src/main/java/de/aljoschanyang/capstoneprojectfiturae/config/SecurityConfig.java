405ad01dc67280397aece15a12ca1572be86f289
Aljoscha Nyang <a.nyang@gmx.de>
2023-12-07 17:28:36 +0100

    Refactor SecurityConfig

--- backend/src/main/java/de/aljoschanyang/capstoneprojectfiturae/config/SecurityConfig.java
+++ backend/src/main/java/de/aljoschanyang/capstoneprojectfiturae/config/SecurityConfig.java
@@ -3,11 +3,13 @@ package de.aljoschanyang.capstoneprojectfiturae.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +27,8 @@ public class SecurityConfig {
                         .anyRequest().permitAll()
                 )
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .exceptionHandling(exceptionHandlingConfigurer ->
+                        exceptionHandlingConfigurer.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
                 .logout(l -> {
                     if(environment.equals("prod")) {
                         l.logoutSuccessUrl("/").permitAll();
