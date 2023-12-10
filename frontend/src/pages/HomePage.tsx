91708096f90e20c6e445aa5bbc37e0b352d2dcd4
Aljoscha Nyang <a.nyang@gmx.de>
2023-12-10 20:52:38 +0100

    Make HomePage responsive

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -17,56 +17,71 @@ function HomePage({userName, imageUrl}:Readonly<HomeProps>) {
 
     return(
         <>
-            <header className={"header-home"}>
-                <div className={"header-home-bg"}></div>
-                <div className={"header-greeting"}>
-                    <img
-                        src={imageUrl}
-                        className={"profile-pic"}
-                        alt={"profile picture"}
-                    />
-                    <p>Hey, <span className={"bold"}>{userName ?? "User"}!</span></p>
-                </div>
-                <div className={"test"}></div>
-            </header>
-            <div className={"position-fix-home"}></div>
-            <div className={"main-wrapper"}>
-                <h2 className={"home-title"}>Recent Workouts</h2>
-                {workouts.map((workout:Workout) => {
-                    return(
-                        <div key={workout.id} className={"workout-card"}>
-                            <div className={"workout-card-head"}>
-                                <h3>{workout.name}</h3>
-                                <p>{workout.day}</p>
-                            </div>
-                            <div className={"workout-card-body"}>
-                                <p>{workout.description}</p>
-                                <p>{workout.plan.length} Exercise(s)</p>
-                            </div>
-                            <button
-                                className={"icon-bottom-right btn-details"}
-                                onClick={() => workout.id && handleClick(workout.id)}
-                                onKeyDown={(event) => {
-                                    if(event.key === "Enter") {
-                                        workout.id && handleClick(workout.id);
-                                    }
-                                }}
-                                aria-labelledby={`workoutName-${workout.id}`}
-                            >
-                                <RightArrow />
+            <div className={"full-width-container"}>
+                <header className={"header-home"}>
+                    <div className={"header-home-bg"}></div>
+                    <div className={"header-greeting"}>
+                        <img
+                            src={imageUrl}
+                            className={"profile-pic"}
+                            alt={"profile picture"}
+                        />
+                        <div className={"header-home-profile"}>
+                            <p>Hey <span className={"bold"}>{userName ?? "User"}</span></p>
+                            <button className={"btn-home-header-profile"} onClick={() => navigate("/profile")}>
+                                <ProfileIcon/>
                             </button>
                         </div>
-                    )
-                })}
+                    </div>
+                    <div className={"test"}></div>
+                </header>
             </div>
-            <div className={"footer-home"}>
-                <button className={"btn-home-page btn-home-page-add"} onClick={() => navigate("/workout/add")}>
-                    <PlusIcon />
-                </button>
-                <button className={"btn-home-page btn-home-page-profile"} onClick={() => navigate("/profile")}>
-                    <ProfileIcon/>
-                </button>
+            <div className={"position-fix-home"}></div>
+            <div className={"main-wrapper"}>
+                <div className={"main-wrapper-home"}>
+                    <h2 className={"home-title"}>Recent Workouts</h2>
+                    {workouts.map((workout:Workout) => {
+                        return(
+                            <div key={workout.id} className={"workout-card"}>
+                                <div className={"workout-card-head"}>
+                                    <h3>{workout.name}</h3>
+                                    <p>{workout.day}</p>
+                                </div>
+                                <div className={"workout-card-body"}>
+                                    <p>{workout.description}</p>
+                                    <p>{workout.plan.length} Exercise(s)</p>
+                                </div>
+                                <button
+                                    className={"icon-bottom-right btn-details"}
+                                    onClick={() => workout.id && handleClick(workout.id)}
+                                    onKeyDown={(event) => {
+                                        if(event.key === "Enter") {
+                                            workout.id && handleClick(workout.id);
+                                        }
+                                    }}
+                                    aria-labelledby={`workoutName-${workout.id}`}
+                                >
+                                    <RightArrow />
+                                </button>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
+            <footer className={"footer-home-container"}>
+                <div className={"footer-home"}>
+                    <button className={"btn-home-page btn-home-page-add"} onClick={() => navigate("/workout/add")}>
+                        <PlusIcon />
+                    </button>
+                    <button className={"btn-home-page btn-home-page-profile"} onClick={() => navigate("/profile")}>
+                        <ProfileIcon/>
+                    </button>
+                </div>
+                <div className={"footer-single-child"}>
+                    <button className={"btn-bottom-center-fixed btn-regular"} onClick={() => navigate("/workout/add")}>+</button>
+                </div>
+
+            </footer>
         </>
     )
 }
