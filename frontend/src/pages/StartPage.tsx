82eec4bf9205a5baa5d1c004114f06c741f06ae7
Aljoscha Nyang <a.nyang@gmx.de>
2023-12-11 10:18:35 +0100

    Edit StartPage

--- frontend/src/pages/StartPage.tsx
+++ frontend/src/pages/StartPage.tsx
@@ -1,12 +1,26 @@
 import {useNavigate} from "react-router-dom";
+import "./StartPage.css";
 
 function StartPage () {
     const navigate = useNavigate();
 
     return (
         <>
-            <h2>Start Page</h2>
-            <button onClick={() => navigate("/home")}>GET STARTED</button>
+            <header className={"header-start"}>
+                <p className={"logo"}>Fiturae</p>
+                <button className={"header-btn-login"} onClick={() => navigate("/home")}>Login</button>
+            </header>
+            <main>
+                <section className={"banner"}>
+                    <p>Deine Fitness.</p>
+                    <p>Deine Kontrolle.</p>
+                    <p>Dein Erfolg.</p>
+                </section>
+                <div className={"center"}>
+                    <button className={"get-started"} onClick={() => navigate("/home")}>GET STARTED</button>
+                </div>
+
+            </main>
         </>
     )
 }
